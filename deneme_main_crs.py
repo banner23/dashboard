fc81fcf76e467e6bd61e112600f3ef4613c89aa2
banner23 <53205156+banner23@users.noreply.github.com>
2021-03-10 11:31:58 +0300

    Update deneme_main_crs.py

--- deneme_main_crs.py
+++ deneme_main_crs.py
@@ -16,7 +16,6 @@ import math
 power_mgmt_1 = 0x6b
 power_mgmt_2 = 0x6c
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
@@ -27,6 +26,8 @@ class MainWindow(QMainWindow):
         self.ui.sliderSolAlt.valueChanged.connect(self.dikey_egim_sol)
         self.ui.sliderSagUst.valueChanged.connect(self.yanal_egim_sag)
         self.ui.sliderSagAlt.valueChanged.connect(self.dikey_egim_sag)
+
+        
 
         ####### EGİM GOSTERGE ÇİZGİLERİ #########
         labelCizgiSolU10 = QLabel(self.ui.egimAnaFrame)
@@ -255,67 +256,10 @@ class MainWindow(QMainWindow):
 
     ####### EGİM GOSTERGE ÇİZGİLERİ END #########
 
+
         self.show()
 
-    def read_byte(self):
-     return bus.read_byte_data(address, self)
-
-    def read_word(self):
-        high = bus.read_byte_data(address, self)
-        low = bus.read_byte_data(address, self+1)
-        val = (high << 8) + low
-        return val
-
-    def read_word_2c(self):
-        val = read_word(self)
-        if (val >= 0x8000):
-            return -((65535 - val) + 1)
-        else:
-            return val
-
-    def dist(self, b):
-        return math.sqrt((self*self)+(b*b))
-
-    def get_y_rotation(self, y, z):
-        radians = math.atan2(self, dist(y, z))
-        return -math.degrees(radians)
-
-    def get_x_rotation(self, y, z):
-        radians = math.atan2(y, dist(self, z))
-        return math.degrees(radians)
-
-    bus = smbus2.SMBus(1)
-    address = 0x68  # MPU6050 I2C adresi
-
-    #MPU6050 ilk calistiginda uyku modunda oldugundan, calistirmak icin asagidaki komutu veriyoruz:
-    bus.write_byte_data(address, power_mgmt_1, 0)
-
-    gyro_xout = read_word_2c(0x43)
-    gyro_yout = read_word_2c(0x45)
-    gyro_zout = read_word_2c(0x47)
-
-    print("Jiroskop X : ", gyro_xout, " olcekli: ", (gyro_xout / 131))
-    print("Jiroskop Y : ", gyro_yout, " olcekli: ", (gyro_yout / 131))
-    print("Jiroskop Z: ", gyro_zout, " olcekli: ", (gyro_zout / 131))
-
-    #Ivmeolcer register'larini oku
-    accel_xout = read_word_2c(0x3b)
-    accel_yout = read_word_2c(0x3d)
-    accel_zout = read_word_2c(0x3f)
-
-    accel_xout_scaled = accel_xout / 16384.0
-    accel_yout_scaled = accel_yout / 16384.0
-    accel_zout_scaled = accel_zout / 16384.0
-
-    print("Ivmeolcer X: ", accel_xout, " olcekli: ", accel_xout_scaled)
-    print("Ivmeolcer Y: ", accel_yout, " olcekli: ", accel_yout_scaled)
-    print("Ivmeolcer Z: ", accel_zout, " olcekli: ", accel_zout_scaled)
-
-    print("X dondurme: ", get_x_rotation(
-        accel_xout_scaled, accel_yout_scaled, accel_zout_scaled))
-    print("Y dondurme: ", get_y_rotation(
-        accel_xout_scaled, accel_yout_scaled, accel_zout_scaled))
-
+    
     def yanal_egim_sol(self):
         value = self.ui.sliderSolUst.value()
         egim = (100-value) / 100
@@ -415,6 +359,9 @@ class MainWindow(QMainWindow):
         self.ui.dikey_egim_resim.setPixmap(
             QPixmap(u"E:\QT Çalışmaları\son_dashboard\kobrayan.png").transformed(Rightmatrix))
 
+        
+
+
     def dikey_egim_sag(self):
         value = self.ui.sliderSagAlt.value()
         #egim = value / 100
@@ -451,7 +398,69 @@ class MainWindow(QMainWindow):
             QPixmap(u"E:\QT Çalışmaları\son_dashboard\kobrayan.png").transformed(Rightmatrix))
 
 
+
 if __name__ == "__main__":
+
+    def read_byte(self):
+     return bus.read_byte_data(address, self)
+
+    def read_word(self):
+        high = bus.read_byte_data(address, self)
+        low = bus.read_byte_data(address, self+1)
+        val = (high << 8) + low
+        return val
+
+    def read_word_2c(self):
+        val = read_word(self)
+        if (val >= 0x8000):
+            return -((65535 - val) + 1)
+        else:
+            return val
+
+    def dist(self, b):
+        return math.sqrt((self*self)+(b*b))
+
+    def get_y_rotation(self, y, z):
+        radians = math.atan2(self, dist(y, z))
+        return -math.degrees(radians)
+
+    def get_x_rotation(self, y, z):
+        radians = math.atan2(y, dist(self, z))
+        return math.degrees(radians)
+
+    bus = smbus.SMBus(1)
+    address = 0x68  # MPU6050 I2C adresi
+
+    #MPU6050 ilk calistiginda uyku modunda oldugundan, calistirmak icin asagidaki komutu veriyoruz:
+    bus.write_byte_data(address, power_mgmt_1, 0)
+
+    gyro_xout = read_word_2c(0x43)
+    gyro_yout = read_word_2c(0x45)
+    gyro_zout = read_word_2c(0x47)
+
+    print("Jiroskop X : ", gyro_xout, " olcekli: ", (gyro_xout / 131))
+    print("Jiroskop Y : ", gyro_yout, " olcekli: ", (gyro_yout / 131))
+    print("Jiroskop Z: ", gyro_zout, " olcekli: ", (gyro_zout / 131))
+
+    #Ivmeolcer register'larini oku
+    accel_xout = read_word_2c(0x3b)
+    accel_yout = read_word_2c(0x3d)
+    accel_zout = read_word_2c(0x3f)
+
+    accel_xout_scaled = accel_xout / 16384.0
+    accel_yout_scaled = accel_yout / 16384.0
+    accel_zout_scaled = accel_zout / 16384.0
+
+    print("Ivmeolcer X: ", accel_xout, " olcekli: ", accel_xout_scaled)
+    print("Ivmeolcer Y: ", accel_yout, " olcekli: ", accel_yout_scaled)
+    print("Ivmeolcer Z: ", accel_zout, " olcekli: ", accel_zout_scaled)
+
+    print("X dondurme: ", get_x_rotation(
+        accel_xout_scaled, accel_yout_scaled, accel_zout_scaled))
+    print("Y dondurme: ", get_y_rotation(
+        accel_xout_scaled, accel_yout_scaled, accel_zout_scaled))
+
+
     app = QApplication(sys.argv)
     window = MainWindow()
     sys.exit(app.exec_())
